0f2a175366d192c5fd204c857be48c9cfb2125f2
Cherlia Fleming <cherlia91@gmail.com>
2026-03-14 09:23:28 -0500

    added pictures to fish cards

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -6,7 +6,7 @@ export const database = {
             length: 3,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/nemo.jpg"
+            image: "https://t4.ftcdn.net/jpg/07/34/94/39/240_F_734943970_bUNbIZxDeyKjnaXg2chiLXVyvqwBo1WI.jpg"
         },
         {
             name: "Sharky",
@@ -14,7 +14,7 @@ export const database = {
             length: 15,
             location: "Great Barrier Reef",
             diet: "Carnivore",
-            image: "https://example.com/images/sharky.jpg"
+            image: "https://t4.ftcdn.net/jpg/17/15/81/23/240_F_1715812324_OGQLlx9ExgH26xIH7jyhQnijMl6XOwnJ.jpg"
         },
         {
             name: "Dory",
@@ -22,7 +22,7 @@ export const database = {
             length: 5,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/dory.jpg"
+            image: "https://t3.ftcdn.net/jpg/01/01/66/12/240_F_101661230_qgfjeid1Q6g9KL6D2163DHsxAdYWPzf6.jpg"
         },
         {   
             name: "Bruce",
@@ -30,7 +30,7 @@ export const database = {
             length: 15,
             location: "Great Barrier Reef",
             diet: "Carnivore",
-            image: "https://example.com/images/bruce.jpg"
+            image: "https://t3.ftcdn.net/jpg/00/78/11/08/240_F_78110815_a8rvqtihW19tkT3hO5NYAjAapbKEBxdc.jpg"
         },
         {
             name: "Barry",
@@ -38,7 +38,7 @@ export const database = {
             length: 15,
             location: "Great Barrier Reef",
             diet: "Carnivore",
-            image: "https://example.com/images/barry.jpg"
+            image: "https://t4.ftcdn.net/jpg/05/66/26/97/240_F_566269794_0KchbKyqc0KMptf2arTOxrdBWX6OTV8t.jpg"
         },
         {
             name:"Angel",
@@ -46,95 +46,95 @@ export const database = {
             length: 4,
             location: "Great Barrier Reef",
             diet: "Herbivore",
-            image: "https://example.com/images/angel.jpg"
+            image: "https://t3.ftcdn.net/jpg/04/31/24/80/240_F_431248018_SFGF0ovUNQTbvS0OazR4RQtx7KDensT6.jpg"
         },
         {
             name: "Lord Farquad",
-            species: "Guppy",
+            species: "Salmon",
             length: 2,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/lordfarquad.jpg"
+            image: "https://t4.ftcdn.net/jpg/01/02/03/01/240_F_102030111_bfjxaCiSIPBkqhzo7A7lNNlTqf2etohL.jpg"
         },
         {
             name: "Beyonce",
-            species: "Goldfish",
+            species: "Tiger Barb",
             length: 1,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/beyonce.jpg"
+            image: "https://t4.ftcdn.net/jpg/00/13/14/93/240_F_13149326_Hk1SLnacRlE6TewmKlq7846IAiwS92vG.jpg"
         },
         {
             name: "Linda",
             species: "Jellyfish",
             length: 5,
             location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/bubbles.jpg"
+            diet: "Carnivore",
+            image: "https://t3.ftcdn.net/jpg/04/94/49/08/240_F_494490869_gC0o2Ivij46vMcY4M2QQ2TjV8NaydEMl.jpg"
         },
         {
             name: "Mr. Tee",
-            species: "Jellyfish",
+            species: "Angelfish",
             length: 5,            
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/mrtee.jpg"   
+            image: "https://t3.ftcdn.net/jpg/05/79/06/24/240_F_579062439_jNMtXBcY3FrHbOhRDtAqvcWpeHTkKJXI.jpg"   
         },
         {
             name: "Princess",
-            species: "Jellyfish",
+            species: "Gouramis",
             length: 5,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/bubbles.jpg"
+            image: "https://t4.ftcdn.net/jpg/00/61/85/89/240_F_61858959_vbsXHZpP15NITrrrX3OZKlEikmPC04lm.jpg"
         },
         {
             name: "Nadine",
-            species: "Jellyfish",
+            species: "Swordtail",
             length: 5,            
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/mrtee.jpg"   
+            image: "https://t4.ftcdn.net/jpg/15/65/62/49/240_F_1565624930_N5Nv6Yjsm6L60zVElMHbqDpGXtR5zkAk.jpg"   
         },
         {
             name: "Chaka Khan",
-            species: "Jellyfish",
+            species: "Loach",
             length: 5,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/bubbles.jpg"
+            image: "https://t3.ftcdn.net/jpg/00/67/63/30/240_F_67633012_oSKs61AiFxjYwYzaqNTzig3vfMk7rN2W.jpg"
         },
         {
             name: "The Rock",
-            species: "Jellyfish",
+            species: "Piranha",
             length: 5,            
             location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/mrtee.jpg"   
+            diet: "Carnivore",
+            image: "https://t4.ftcdn.net/jpg/01/52/27/33/240_F_152273358_HPUJuO2pLG3xhir2fwquYuFCzP6LqE6x.jpg"   
         },
         {
             name: "Mr. Incredible",
-            species: "Jellyfish",
+            species: "Barracuda",
             length: 5,
             location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/bubbles.jpg"            
+            diet: "Carnivore",
+            image: "https://t3.ftcdn.net/jpg/01/97/33/70/240_F_197337044_Cmzcv3UTG7pdQGeFX5G0br15nfTpsVNu.jpg"            
         },
         {
             name: "Will Ferrell",
-            species: "Jellyfish",
+            species: "Lionfish",
             length: 5,            
             location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/mrtee.jpg"            
+            diet: "Carnivore",
+            image: "https://t3.ftcdn.net/jpg/15/61/37/96/240_F_1561379618_l54UfEYleE3PlGZRZPLIKwQYOFOZ2boa.jpg"            
         },
         {
             name: "Pochahantas",
-            species: "Jellyfish",
+            species: "Pike fish",
             length: 5,
             location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/bubbles.jpg"
+            diet: "Carnivore",
+            image: "https://t3.ftcdn.net/jpg/05/73/18/88/240_F_573188825_jOQPWw0pWtbQl242ZPnSZWzW3e177ekA.jpg"
         }
     ],
     tips: [
@@ -148,6 +148,21 @@ export const database = {
         name: "Great Barrier Reef",
         country: "Australia",
         description: "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse array of marine life."
+        },
+        {
+            name: "Bali",
+            country: "Indonesia",
+            description: "Bali is known for its stunning beaches, lush rainforests, and vibrant culture. It is a popular destination for tourists and locals alike."        
+        },
+        {
+            name: "Hawaii",
+            country: "United States",
+            description: "Hawaii is a tropical paradise known for its stunning beaches, lush rainforests, and vibrant culture. It is a popular destination for tourists and locals alike."
+        },
+        {   
+            name: "Maldives",
+            country: "Maldives",
+            description: "The Maldives is a tropical paradise known for its stunning beaches, lush rainforests, and vibrant culture. It is a popular destination for tourists and locals alike."
         }
     ]
 }
@@ -170,6 +185,7 @@ export const soldierFish = () => {
     return soldierFish
 }
 export const regularFish = () => {
+    const regularFish = []
     database.fish.forEach(fish => {
         if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
             regularFish.push(fish)
@@ -178,3 +194,4 @@ export const regularFish = () => {
 
     return regularFish  
 }
+
--- scripts/fishList.js
+++ scripts/fishList.js
@@ -1,15 +1,24 @@
 import { database } from './aquariumData.js';
 
 export const fishList = () => {
-   const ul = document.createElement('ul');
-   ul.className = 'fish';
-   database.fish.forEach(fish => {
-       const li = document.createElement('li');
-       li.className = 'fish-card';
-       li.textContent = fish.name;
-       ul.appendChild(li);
-   });
-   return ul;           
+    const ul = document.createElement('ul');
+    ul.className = 'fish';
 
+    database.fish.forEach(fish => {
+        const li = document.createElement('li');
+        li.className = 'fish-card';
+
+        const img = document.createElement('img');
+        img.src = fish.image;
+        img.alt = fish.name;
+
+        const name = document.createElement('p');
+        name.textContent = fish.name;
+
+        li.appendChild(img);    // ← add image to card
+        li.appendChild(name);   // ← add name to card
+        ul.appendChild(li);     // ← add card to list
+    });
+
+    return ul;
 };
-
