0576fe2862b18848e57c04e26643c4550bd8fb12
Cherlia Fleming <cherlia91@gmail.com>
2026-03-28 11:04:11 -0500

    add PotteryCatalog module

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,20 @@
+const catalog = []
+
+export const toSellOrNotToSell = (pottery) => {
+
+    if (pottery.cracked) {
+        return 
+    }
+    if (pottery.weight >=6) {
+        pottery.price = 40 
+    }
+     else if (pottery.weight < 6) {
+        pottery.price = 20 
+    }
+    catalog.push(pottery)
+    return pottery
+}
+
+export const usePottery = () => {
+return structuredClone(catalog)
+} 
